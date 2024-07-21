d7e963d8010aeb7f576c9928fd78d4a81f1625d7
mahdi <mahdimehrabi17@gmail.com>
2024-07-21 14:08:37 +0330

    save image content actually

--- service/saver.go
+++ service/saver.go
@@ -8,6 +8,8 @@ import (
 	"github.com/minio/minio-go/v7"
 	"github.com/rabbitmq/amqp091-go"
 	"log/slog"
+	"rb1-downloader/entity"
+	"rb1-downloader/util"
 	"time"
 )
 
@@ -27,23 +29,33 @@ func NewSaver(minC *minio.Client, logger *slog.Logger, ch *amqp091.Channel, queu
 		queueName: queueName,
 	}
 
-	for range 50 {
-		go s.Worker()
-	}
 	return s
 }
 
 func (s *Saver) Setup() error {
 	deliveries, err := s.ch.Consume(s.queueName, "", false, false, false, false, nil)
 	s.queue = deliveries
+	for range 50 {
+		go s.Worker()
+	}
 	return err
 }
 
 func (s *Saver) Worker() {
 	for msg := range s.queue {
-		reader := bytes.NewReader(msg.Body)
+		url, err := entity.FromJSON(msg.Body)
+		if err != nil {
+			s.logger.Error("Error:%s", err.Error())
+			return
+		}
+		content, err := util.DownloadImage(url.URL)
+		if err != nil {
+			s.logger.Error("Error:%s", err.Error())
+			return
+		}
+		reader := bytes.NewReader(content)
 		id := uuid.New().String()
-		_, err := s.minC.PutObject(context.Background(), "images", id, reader, int64(reader.Len()), minio.PutObjectOptions{})
+		_, err = s.minC.PutObject(context.Background(), "images", id, reader, int64(reader.Len()), minio.PutObjectOptions{})
 		if err != nil {
 			if err := msg.Nack(false, false); err != nil {
 				s.logger.Error("failed to nack", err.Error())
@@ -55,8 +67,8 @@ func (s *Saver) Worker() {
 			s.logger.Error("failed to ack", err.Error())
 			if err := s.minC.RemoveObject(context.Background(), "images", id, minio.RemoveObjectOptions{}); err != nil {
 				s.logger.Error("failed to remove object", err.Error())
-				return
 			}
+			return
 		}
 		fmt.Println("saved successfully", time.Now().UnixNano())
 	}
